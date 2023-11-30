ea41b45c36db1afe437b892746a0315237aaf5fb
Sophia <111466302+MintyLuve@users.noreply.github.com>
2023-11-30 17:37:27 -0500

    facts page completed

--- app/src/main/java/com/fro/parade360/FactsActivity.java
+++ app/src/main/java/com/fro/parade360/FactsActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FactsActivity extends AppCompatActivity {
 
     Button backButton;
+    ImageButton icon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,19 @@ public class FactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_facts);
 
         backButton = (Button) findViewById(R.id.back);
+        icon = (ImageButton) findViewById(R.id.dreamworks);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+            }
+        });
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DreamworksActivity.class));
             }
         });
 
